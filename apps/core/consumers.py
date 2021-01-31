c46a6702f11153e6e9a42a8fc7e1104b32301975
Vladimir Shturmer <cybersturmer@ya.ru>
2021-02-01 00:43:37 +0300

    Observe sprints.

--- apps/core/consumers.py
+++ apps/core/consumers.py
@@ -2,26 +2,10 @@ from djangochannelsrestframework.consumers import AsyncAPIConsumer
 from djangochannelsrestframework.decorators import action
 from channels.generic.websocket import WebsocketConsumer
 import json
-from .api.serializers import IssueSerializer
-from .models import Issue
+from .api.serializers import IssueSerializer, SprintWritableSerializer
+from .models import Issue, Sprint
 
 from djangochannelsrestframework.observer import model_observer
-
-
-class LiveConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
-
-    def disconnect(self, close_code):
-        pass
-
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        self.send(text_data=json.dumps({
-            'message': f'I got your message "{message}"'
-        }))
 
 
 class IssueConsumerObserver(AsyncAPIConsumer):
@@ -34,5 +18,19 @@ class IssueConsumerObserver(AsyncAPIConsumer):
         await self.send_json(message)
 
     @model_change.serializer
-    def model_serialize(self, instance, action, **kwargs):
+    def model_serialize(self, instance: Issue, action, **kwargs):
         return IssueSerializer(instance).data
+
+
+class SprintConsumerObserver(AsyncAPIConsumer):
+    async def accept(self, **kwargs):
+        await super().accept(** kwargs)
+        await self.model_change.subscribe()
+
+    @model_observer(Sprint)
+    async def model_change(self, message, action=None, **kwargs):
+        await self.send_json(message)
+
+    @model_change.serializer
+    def model_serializer(self, instance: Sprint, action, **kwargs):
+        return SprintWritableSerializer(instance).data
--- apps/core/routing.py
+++ apps/core/routing.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from apps.core.consumers import LiveConsumer, IssueConsumerObserver
+from apps.core.consumers import IssueConsumerObserver, SprintConsumerObserver
 
 websocket_urlpatterns = [
-    path('ws/notifications/', LiveConsumer.as_asgi()),
-    path('ws/issues/', IssueConsumerObserver.as_asgi())
+    path('ws/issues/', IssueConsumerObserver.as_asgi()),
+    path('ws/sprints/', SprintConsumerObserver.as_asgi())
 ]
