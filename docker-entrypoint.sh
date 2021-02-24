a8620f396f0d6e3c8810a6558fc7c66825ec6208
Vladimir Shturmer <cybersturmer@ya.ru>
2021-02-24 22:43:01 +0300

    Updated entrypoint.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,7 +11,7 @@ python manage.py collectstatic --noinput --clear
 
 # Copying api schema to staticfiles (We use it to share on Heroku)
 # We don't need it in production.
-cp -R templates/api staticfiles/api
+cp -R templates/api staticfiles
 
 echo -e "\e[94m Making migrations...\e[0m"
 python manage.py makemigrations
