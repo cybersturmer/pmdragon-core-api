ce6872d13785aa8f5617f56bec216cacf20786f5
Vladimir Shturmer <cybersturmer@ya.ru>
2021-02-24 21:44:18 +0300

    Docker entrypoint update.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,12 +7,17 @@
 # Django (python manage.py dumpdata --all > data.json)
 
 echo -e "\e[94m Collecting static...\e[0m"
-python manage.py collectstatic --clear --noinput
+python manage.py collectstatic --noinput --clear
+
+# Copying api schema to staticfiles (We use it to share on Heroku)
+# We don't need it in production.
+cp -R templates/api staticfiles/api
 
 echo -e "\e[94m Making migrations...\e[0m"
 python manage.py makemigrations
 python manage.py migrate
 
+# For this to work you have to add your own data dump
 echo -e "\e[94m Loading predefined data...\e[0m"
 python manage.py loaddata data.json
 
