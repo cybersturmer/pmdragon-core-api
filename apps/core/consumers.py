c491daef283a175ac01034362d89add962dfd81f
Vladimir Shturmer <cybersturmer@ya.ru>
2021-03-23 00:00:48 +0300

    Super refactored to new calling.

--- apps/core/consumers.py
+++ apps/core/consumers.py
@@ -10,30 +10,6 @@ from .api.serializers import PersonSerializer, IssueMessageSerializer
 from rest_framework.renderers import JSONRenderer
 
 
-class PingConsumer(AsyncConsumer):
-    async def websocket_connect(self, message):
-        await self.send({
-            "type": "websocket.accept"
-        })
-
-    async def websocket_receive(self, message):
-        person: Person = await self.get_person()
-
-        await self.send({
-            "type": "websocket.send",
-            "text": JSONRenderer().render(person)
-        })
-
-    @database_sync_to_async
-    def get_person(self):
-        person = Person.objects.all()[0]
-        serialized = PersonSerializer(person)
-        return serialized.data
-
-    async def websocket_disconnect(self, message):
-        pass
-
-
 class IssueMessagesObserver(AsyncAPIConsumer):
     """
     Payload example
@@ -78,17 +54,13 @@ class IssueMessagesObserver(AsyncAPIConsumer):
 
         return issue
 
-    """
-    Actually we have to check permissions here but we are not able to do that
-    Cuz now we don't have user's data., but we will, i promise. """
     @action()
     async def subscribe_to_messages_in_issue(self, issue_pk, **kwargs):
         try:
             issue = await self.get_issue_filter_data(issue_pk=issue_pk)
             await self.message_change_handler.subscribe(issue=issue)
         except Issue.DoesNotExist:
-            print('Unable to subscribe messages cuz issue was not found')
-
+            print('Unable to subscribe messages cause issue was not found')
 
     @action()
     async def unsubscribe_from_messages_in_issue(self, issue_pk, **kwargs):
@@ -96,4 +68,4 @@ class IssueMessagesObserver(AsyncAPIConsumer):
             issue = await self.get_issue_filter_data(issue_pk=issue_pk)
             await self.message_change_handler.unsubscribe(issue=issue)
         except Issue.DoesNotExist:
-            print('Unable to unsubscribe messages cuz issue was not found')
+            print('Unable to unsubscribe messages cause issue was not found')
