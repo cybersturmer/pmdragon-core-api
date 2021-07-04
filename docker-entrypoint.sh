08e461ea51d89132a6f366c86d02af655e76d3b7
Vladimir Shturmer <cybersturmer@ya.ru>
2021-07-04 03:32:56 +0300

    we don't need this hook anymore.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,10 +3,6 @@
 echo -e "\e[94m Collecting static...\e[0m"
 python manage.py collectstatic --noinput --clear
 
-# Copying api schema to staticfiles (We use it to share on Heroku)
-# We don't need it in production.
-cp -R templates/api staticfiles
-
 echo -e "\e[94m Making migrations...\e[0m"
 python manage.py makemigrations
 python manage.py migrate
