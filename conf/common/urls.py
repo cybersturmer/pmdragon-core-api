c38cc1c4cb9ce5f646aaedc7e5591f3033cb06a9
Vladimir Shturmer <cybersturmer@ya.ru>
2021-07-04 03:25:16 +0300

    Redoc spec added.

--- conf/common/urls.py
+++ conf/common/urls.py
@@ -44,6 +44,8 @@ urlpatterns = [
 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-ui'),
+
     path('api/auth/obtain/',
          TokenObtainPairExtendedView.as_view(),
          name='token_obtain_pair'),
