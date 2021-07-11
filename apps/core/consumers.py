2289942d83b9a68b408ce0e4963c2eac1343d410
Vladimir Shturmer <cybersturmer@ya.ru>
2021-07-11 04:20:38 +0300

    Now we subscribed on all settings in project.

--- apps/core/consumers.py
+++ apps/core/consumers.py
@@ -6,13 +6,24 @@ from djangochannelsrestframework.permissions import IsAuthenticated
 
 from .api.serializers import \
     IssueMessageSerializer, \
-    IssueSerializer
+    IssueSerializer, \
+    IssueTypeSerializer, \
+    IssueTypeIconSerializer, \
+    IssueStateSerializer, \
+    IssueEstimationSerializer
 
 from .models import \
-    Issue,\
-    IssueMessage,\
-    Person,\
+    Issue, \
+    IssueMessage, \
+    IssueTypeCategory, \
+    IssueTypeCategoryIcon, \
+    IssueStateCategory, \
+    IssueEstimationCategory, \
+    Person, \
     Workspace
+
+UNABLE_SUBSCRIBE_NO_WORKSPACE_TEMPLATE = 'Unable to subscribe {obj} cause workspace was not found'
+UNABLE_UNSUBSCRIBE_NO_WORKSPACE_TEMPLATE = 'Unable to unsubscribe {obj} cause workspace was not found'
 
 
 class IssueMessagesObserver(AsyncAPIConsumer):
@@ -68,7 +79,7 @@ class IssueMessagesObserver(AsyncAPIConsumer):
             issue = await self.get_issue_filter_data(issue_pk=issue_pk)
             await self.message_change_handler.subscribe(issue=issue)
         except Issue.DoesNotExist:
-            print('Unable to subscribe messages cause issue was not found')
+            print(UNABLE_SUBSCRIBE_NO_WORKSPACE_TEMPLATE.format(obj='issue'))
 
     @action()
     async def unsubscribe_from_messages_in_issue(self, issue_pk, **kwargs):
@@ -76,7 +87,7 @@ class IssueMessagesObserver(AsyncAPIConsumer):
             issue = await self.get_issue_filter_data(issue_pk=issue_pk)
             await self.message_change_handler.unsubscribe(issue=issue)
         except Issue.DoesNotExist:
-            print('Unable to unsubscribe messages cause issue was not found')
+            print(UNABLE_UNSUBSCRIBE_NO_WORKSPACE_TEMPLATE.format(obj='issue'))
 
 
 class WorkspaceIssuesObserver(AsyncAPIConsumer):
@@ -132,7 +143,7 @@ class WorkspaceIssuesObserver(AsyncAPIConsumer):
             workspace = await self.get_workspace_filter_data(workspace_pk=workspace_pk)
             await self.issue_change_handler.subscribe(workspace=workspace)
         except Workspace.DoesNotExist:
-            print('Unable to subscribe issues cause workspace was not found')
+            print(UNABLE_SUBSCRIBE_NO_WORKSPACE_TEMPLATE.format(obj='issues'))
 
     @action()
     async def unsubscribe_from_issues_in_workspace(self, workspace_pk, **kwargs):
@@ -140,4 +151,212 @@ class WorkspaceIssuesObserver(AsyncAPIConsumer):
             workspace = await self.get_workspace_filter_data(workspace_pk=workspace_pk)
             await self.issue_change_handler.unsubscribe(workspace=workspace)
         except Workspace.DoesNotExist:
-            print('Unable to unsubscribe issues cause workspace was not found')
+            print(UNABLE_UNSUBSCRIBE_NO_WORKSPACE_TEMPLATE.format(obj='issues'))
+
+
+class WorkspaceIssueTypeCategoriesObserver(AsyncAPIConsumer):
+    permission_classes = (
+        IsAuthenticated,
+    )
+
+    @model_observer(IssueTypeCategory)
+    async def issue_type_category_change_handler(self, message, observer=None, action=None, **kwargs):
+        await self.send_json(dict(message=message, action=action))
+
+    @issue_type_category_change_handler.serializer
+    def model_serializer(self, instance: IssueTypeCategory, action=None, **kwargs):
+        return IssueTypeSerializer(instance).data
+
+    @issue_type_category_change_handler.groups_for_signal
+    def issue_type_category_change_handler(self, instance: IssueTypeCategory, **kwargs):
+        yield f'-workspace__{instance.workspace_id}'
+        yield f'-pk__{instance.pk}'
+
+    @issue_type_category_change_handler.groups_for_consumer
+    def issue_type_category_change_handler(self, workspace=None, issue_type=None, **kwargs):
+        if workspace is not None:
+            yield f'-workspace__{workspace.pk}'
+        if issue_type is not None:
+            yield f'-pk__{issue_type.pk}'
+
+    @database_sync_to_async
+    def get_workspace_filter_data(self, workspace_pk, **kwargs):
+        user = self.scope.get('user')
+        person = Person.objects.get(user=user)
+
+        workspace = Workspace.objects.get(id=workspace_pk,
+                                          participants__in=[person])
+
+        return workspace
+
+    @action()
+    async def subscribe_to_issue_type_categories_in_workspace(self, workspace_pk, **kwargs):
+        try:
+            workspace = await self.get_workspace_filter_data(workspace_pk=workspace_pk)
+            await self.issue_type_category_change_handler.subscribe(workspace=workspace)
+        except Workspace.DoesNotExist:
+            print(UNABLE_SUBSCRIBE_NO_WORKSPACE_TEMPLATE.format(obj='issue type category'))
+
+    @action()
+    async def unsubscribe_from_issue_type_categories_in_workspace(self, workspace_pk, **kwargs):
+        try:
+            workspace = await self.get_workspace_filter_data(workspace_pk=workspace_pk)
+            await self.issue_type_category_change_handler.unsubscribe(workspace=workspace)
+        except Workspace.DoesNotExist:
+            print(UNABLE_UNSUBSCRIBE_NO_WORKSPACE_TEMPLATE.format(obj='issue type category'))
+
+
+class WorkspaceIssueTypeCategoriesIconsObserver(AsyncAPIConsumer):
+    permission_classes = (
+        IsAuthenticated,
+    )
+
+    @model_observer(IssueTypeCategoryIcon)
+    async def issue_type_category_icon_change_handler(self, message, observer=None, action=None, **kwargs):
+        await self.send_json(dict(message=message, action=action))
+
+    @issue_type_category_icon_change_handler.serializer
+    def model_serializer(self, instance: IssueTypeCategoryIcon, action=None, **kwargs):
+        return IssueTypeIconSerializer(instance).data
+
+    @issue_type_category_icon_change_handler.groups_for_signal
+    def issue_type_category_icon_change_handler(self, instance: IssueTypeCategoryIcon, **kwargs):
+        yield f'-workspace__{instance.workspace_id}'
+        yield f'-pk__{instance.pk}'
+
+    @issue_type_category_icon_change_handler.groups_for_consumer
+    def issue_type_category_icon_change_handler(self, workspace=None, issue_type_category_icon=None, **kwargs):
+        if workspace is not None:
+            yield f'-workspace__{workspace.pk}'
+        if issue_type_category_icon is not None:
+            yield f'-pk__{issue_type_category_icon.pk}'
+
+    @database_sync_to_async
+    def get_workspace_filter_data(self, workspace_pk, **kwargs):
+        user = self.scope.get('user')
+        person = Person.objects.get(user=user)
+
+        workspace = Workspace.objects.get(id=workspace_pk,
+                                          participants__in=[person])
+
+        return workspace
+
+    @action()
+    async def subscribe_to_issue_type_categories_icons_in_workspace(self, workspace_pk, **kwargs):
+        try:
+            workspace = await self.get_workspace_filter_data(workspace_pk=workspace_pk)
+            await self.issue_type_category_icon_change_handler.subscribe(workspace=workspace)
+        except Workspace.DoesNotExist:
+            print(UNABLE_SUBSCRIBE_NO_WORKSPACE_TEMPLATE.format(obj='issue type icon category'))
+
+    @action()
+    async def unsubscribe_from_issue_type_categories_icons_in_workspace(self, workspace_pk, **kwargs):
+        try:
+            workspace = await self.get_workspace_filter_data(workspace_pk=workspace_pk)
+            await self.issue_type_category_icon_change_handler.unsubscribe(workspace=workspace)
+        except Workspace.DoesNotExist:
+            print(UNABLE_UNSUBSCRIBE_NO_WORKSPACE_TEMPLATE.format(obj='issue type icon category'))
+
+
+class WorkspaceIssueStateCategoriesObserver(AsyncAPIConsumer):
+    permission_classes = (
+        IsAuthenticated,
+    )
+
+    @model_observer(IssueStateCategory)
+    async def issue_state_change_handler(self, message, observer=None, action=None, **kwargs):
+        await self.send_json(dict(message=message, action=action))
+
+    @issue_state_change_handler.serializer
+    def model_serializer(self, instance: IssueStateCategory, action=None, **kwargs):
+        return IssueStateSerializer(instance).data
+
+    @issue_state_change_handler.groups_for_signal
+    def issue_state_change_handler(self, instance: IssueStateCategory, **kwargs):
+        yield f'-workspace__{instance.workspace_id}'
+        yield f'-pk__{instance.pk}'
+
+    @issue_state_change_handler.groups_for_consumer
+    def issue_state_change_handler(self, workspace=None, issue_state=None, **kwargs):
+        if workspace is not None:
+            yield f'-workspace__{workspace.pk}'
+        if issue_state is not None:
+            yield f'-pk__{issue_state.pk}'
+
+    @database_sync_to_async
+    def get_workspace_filter_data(self, workspace_pk, **kwargs):
+        user = self.scope.get('user')
+        person = Person.objects.get(user=user)
+
+        workspace = Workspace.objects.get(id=workspace_pk,
+                                          participants__in=[person])
+
+        return workspace
+
+    @action()
+    async def subscribe_to_issue_state_categories_in_workspace(self, workspace_pk, **kwargs):
+        try:
+            workspace = await self.get_workspace_filter_data(workspace_pk=workspace_pk)
+            await self.issue_state_change_handler.subscribe(workspace=workspace)
+        except Workspace.DoesNotExist:
+            print(UNABLE_SUBSCRIBE_NO_WORKSPACE_TEMPLATE.format(obj='issue state category'))
+
+    @action()
+    async def unsubscribe_from_issue_state_categories_in_workspace(self, workspace_pk, **kwargs):
+        try:
+            workspace = await self.get_workspace_filter_data(workspace_pk=workspace_pk)
+            await self.issue_state_change_handler.unsubscribe(workspace=workspace)
+        except Workspace.DoesNotExist:
+            print(UNABLE_UNSUBSCRIBE_NO_WORKSPACE_TEMPLATE.format(obj='issue type category'))
+
+
+class WorkspaceIssueEstimationCategoriesObserver(AsyncAPIConsumer):
+    permission_classes = (
+        IsAuthenticated,
+    )
+
+    @model_observer(IssueEstimationCategory)
+    async def issue_estimation_category_change_handler(self, message, observer=None, action=None, **kwargs):
+        await self.send_json(dict(message=message, action=action))
+
+    @issue_estimation_category_change_handler.serializer
+    def model_serializer(self, instance: IssueEstimationCategory, action=None, **kwargs):
+        return IssueEstimationSerializer(instance).data
+
+    @issue_estimation_category_change_handler.groups_for_signal
+    def issue_estimation_category_change_handler(self, instance: IssueEstimationCategory, **kwargs):
+        yield f'-workspace__{instance.workspace_id}'
+        yield f'-pk__{instance.pk}'
+
+    @issue_estimation_category_change_handler.groups_for_consumer
+    def issue_estimation_category_change_handler(self, workspace=None, issue_estimation=None, **kwargs):
+        if workspace is not None:
+            yield f'-workspace__{workspace.pk}'
+        if issue_estimation is not None:
+            yield f'-pk__{issue_estimation.pk}'
+
+    @database_sync_to_async
+    def get_workspace_filter_data(self, workspace_pk, **kwargs):
+        user = self.scope.get('user')
+        person = Person.objects.get(user=user)
+
+        workspace = Workspace.objects.get(id=workspace_pk,
+                                          participants__in=[person])
+
+        return workspace
+
+    @action()
+    async def subscribe_to_issue_estimation_categories_in_workspace(self, workspace_pk, **kwargs):
+        try:
+            workspace = await self.get_workspace_filter_data(workspace_pk=workspace_pk)
+            await self.issue_estimation_category_change_handler.subscribe(workspace=workspace)
+        except Workspace.DoesNotExist:
+            print(UNABLE_SUBSCRIBE_NO_WORKSPACE_TEMPLATE.format(obj='issue estimation category'))
+
+    @action()
+    async def unsubscribe_from_issue_estimation_categories_in_workspace(self, workspace_pk, **kwargs):
+        try:
+            workspace = await self.get_workspace_filter_data(workspace_pk=workspace_pk)
+            await self.issue_estimation_category_change_handler.unsubscribe(workspace=workspace)
+        except Workspace.DoesNotExist:
+            print(UNABLE_UNSUBSCRIBE_NO_WORKSPACE_TEMPLATE.format(obj='issue estimation category'))
--- apps/core/multiplexer.py
+++ apps/core/multiplexer.py
@@ -1,12 +1,22 @@
 from channelsmultiplexer import AsyncJsonWebsocketDemultiplexer
-from .consumers import IssueMessagesObserver, WorkspaceIssuesObserver
+from .consumers import \
+    IssueMessagesObserver, \
+    WorkspaceIssuesObserver, \
+    WorkspaceIssueTypeCategoriesObserver, \
+    WorkspaceIssueTypeCategoriesIconsObserver, \
+    WorkspaceIssueStateCategoriesObserver, \
+    WorkspaceIssueEstimationCategoriesObserver
 
 
 class MultiplexerAsyncJson(AsyncJsonWebsocketDemultiplexer):
     """
-    {"stream":"ping","payload":{"text":"Hello world"}}
+    {"stream":"issue_chat","payload":{"text":"Hello world"}}
     """
     applications = {
         'issue_chat': IssueMessagesObserver.as_asgi(),
-        'workspace_issues': WorkspaceIssuesObserver.as_asgi()
+        'workspace_issues': WorkspaceIssuesObserver.as_asgi(),
+        'workspace_issue_types': WorkspaceIssueTypeCategoriesObserver.as_asgi(),
+        'workspace_issue_type_icons': WorkspaceIssueTypeCategoriesIconsObserver.as_asgi(),
+        'workspace_issue_states': WorkspaceIssueStateCategoriesObserver.as_asgi(),
+        'workspace_issue_estimations': WorkspaceIssueEstimationCategoriesObserver.as_asgi(),
     }
