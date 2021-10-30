f01ce9406c893d635a9fc024532e5b30a70bcc2e
Vladimir Shturmer <cybersturmer@ya.ru>
2021-10-30 20:02:03 +0300

    Project_number to number.

--- apps/core/api/serializers.py
+++ apps/core/api/serializers.py
@@ -758,7 +758,7 @@ class IssueSerializer(WorkspaceModelSerializer):
 		model = Issue
 		fields = (
 			'id',
-			'project_number',
+			'number',
 			'workspace',
 			'project',
 			'title',
@@ -775,7 +775,7 @@ class IssueSerializer(WorkspaceModelSerializer):
 			'ordering',
 		)
 		extra_kwargs = {
-			'project_number': {'read_only': True},
+			'number': {'read_only': True},
 			'created_by': {'read_only': True},
 			'created_at': {'read_only': True},
 			'type_category': {'required': False},
