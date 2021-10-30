9b19f9dc5aa0b6bd769c815c72833f5beb52716c
Vladimir Shturmer <cybersturmer@ya.ru>
2021-10-31 02:13:09 +0300

    We dont need workspace and project here.

--- apps/core/api/serializers.py
+++ apps/core/api/serializers.py
@@ -809,8 +809,6 @@ class IssueHistorySerializer(serializers.ModelSerializer):
 		model = IssueHistory
 		fields = (
 			'id',
-			'workspace',
-			'project',
 			'entry_type',
 			'edited_field',
 			'before_value',
