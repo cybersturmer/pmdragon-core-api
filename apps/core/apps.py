b79576cd0592f1fbef2d86df98a4c38ff3f060d8
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-02-05 17:58:39 +0300

    Just useless comment added.

--- apps/core/apps.py
+++ apps/core/apps.py
@@ -3,10 +3,11 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class CoreConfig(AppConfig):
-    name = 'apps.core'
-    label = 'core'
-    verbose_name = _('Core')
-    default_auto_field = 'django.db.models.BigAutoField'
+	name = 'apps.core'
+	label = 'core'
+	verbose_name = _('Core')
+	default_auto_field = 'django.db.models.BigAutoField'
 
-    def ready(self):
-        from apps.core import signals
+	def ready(self):
+		# Strange way to import signals and activate them.
+		from apps.core import signals
